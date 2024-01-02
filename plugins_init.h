1feb4e35cf1409eed909540e328e2cff4df1ab1f
andrew_marles <andrew@expatria.ca>
2024-01-01 18:19:17 -0800

    Removed extra init from plugins_init.h

--- plugins_init.h
+++ plugins_init.h
@@ -141,11 +141,6 @@
     probe_relay_init();
 #endif
 
-#if PROBE_PROTECT_ENABLE
-    extern void probe_protect_init (void);
-    probe_protect_init();
-#endif
-
 #if DISPLAY_ENABLE
     void display_init (void);
     display_init();
